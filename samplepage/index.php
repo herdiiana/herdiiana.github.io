1729be52c52d16435f6142c74db888642e051ef9
herdiiana <daniherdiana07@gmail.com>
2019-07-25 09:59:03 +0700

    first commit

---
+++ samplepage/index.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+<!-- mycss -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>SMCC Utama Indonesia</title>
+      <link rel="shortcut icon" href="smcc.png">
+    </head>
+
+    <body>
+
+<!-- navbar -->
+      
+
+      <nav class="blue darken-2">
+        <div class="container">
+        <div class="nav-wrapper">
+          <img class="brand-logo" src="img/logo/new3.png">
+          <!-- <a href="#!" class="brand-logo" src="img/logo/smcc.png"></a> -->
+          <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="">Home</a></li>
+            <li><a href="">Our Company</a></li>
+            <li><a href="">Our Services</a></li>
+            <li><a href="">Projects</a></li>
+          
+            <li><a href="">Career</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+        </div>
+      </nav>
+    </div>
+
+
+<!-- sidenav -->
+    <ul class="sidenav" id="mobile-nav">
+      <li><a href="">About</a></li>
+      <li><a href="">Client</a></li>
+      <li><a href="">Service</a></li>
+      <li><a href="">Portfolio</a></li>
+      <li><a href="">Contact Us</a></li>
+    </ul>
+
+
+<!-- slider -->
+    <div class="slider">
+      <ul class="slides">
+        <li>
+          <img src="img/slider/new1.jpg"> 
+          <div class="caption center-align">
+            <h3 class="black-text ">OFFICE AND WAREHOUSE BUILDING</h3>
+            <!-- <h5 class="light grey-text text-lighten-3">NEX LOGICTICS </h5> -->
+          </div>
+        </li>
+        <li>
+          <img src="img/slider/new2.jpg"> 
+          <div class="caption center-align">
+            <h3 class="black-text ">OFFICE AND PRODUCTION BUILDING</h3>
+            <!-- <h5 class="light grey-text text-lighten-3">HOYU</h5> -->
+          </div>
+        </li>
+        <li>
+          <img src="img/slider/new3.jpg"> 
+          <div class="caption center-align">
+            <h3 class="black-text ">OFFICE AND PRODUCTION BUILDING</h3>
+            <!-- <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5> -->
+          </div>
+        </li>
+      </ul>
+    </div>
+
+
+<!-- about us -->
+  <section id="about" class="about">
+    <div class="container">
+      <div class="row">
+        <h3 class="center  grey-text text-darken-3">Project Management System</h3>
+        <div class="col m3 light">
+          <h5 class="center">Quality</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad dignissimos rerum laborum. Soluta deserunt possimus in tenetur nihil deleniti? A sunt perspiciatis quisquam ea doloribus, ipsum vitae ullam cumque!</p>
+        </div>
+
+        <div class="col m3 light">
+          <h5 class="center">Health</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad dignissimos rerum laborum. Soluta deserunt possimus in tenetur nihil deleniti? A sunt perspiciatis quisquam ea doloribus, ipsum vitae ullam cumque!</p>
+        </div>
+        <div class="col m3 light">
+          <h5 class="center">Safety</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad dignissimos rerum laborum. Soluta deserunt possimus in tenetur nihil deleniti? A sunt perspiciatis quisquam ea doloribus, ipsum vitae ullam cumque!</p>
+        </div>
+        <div class="col m3 light">
+          <h5 class="center">Environment</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ad dignissimos rerum laborum. Soluta deserunt possimus in tenetur nihil deleniti? A sunt perspiciatis quisquam ea doloribus, ipsum vitae ullam cumque!</p>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+
+
+<!-- objective -->
+  <section id="portfolio" class="portfolio blue lighten-5">
+    <div class="container">
+      <h3 class ="center grey-text text-darken-3">Objective & Policy</h3>
+      <div class="row">
+        <div class="col m3 s12">
+          <img src="img/policy/2015 POLICY-Q.jpg" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/policy/2015 POLICY-S.jpg" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/policy/2015 OBJECTIVE-Q.jpg" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/policy/2015 OBJECTIVE-S.jpg" class="responsive-img materialboxed">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <section id="portfolio" class="portfolio">
+    <div class="container">
+      <h3 class ="center grey-text text-darken-3">Certification</h3>
+      <div class="row">
+        <div class="col m4 s12">
+          <img src="img/policy/2015 POLICY-Q.jpg" class="responsive-img materialboxed">
+        </div>
+        <div class="col m4 s12">
+          <img src="img/policy/2015 POLICY-S.jpg" class="responsive-img materialboxed">
+        </div>
+        <div class="col m4 s12">
+          <img src="img/policy/2015 POLICY-S.jpg" class="responsive-img materialboxed">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+    <!-- paralax -->
+    <!-- <div class="parallax-container"> -->
+      <!-- <div class="parallax"><img src="img/slider/4.png"></div> -->
+
+      <!-- <div class="container clients">
+        <h3 class="center light white-text">Our Client</h3>
+        <div class="row">
+          <div class="col m4 s12 center">
+            <img src="img/clients/gojek.png">
+          </div>
+          <div class="col m4 s12 center">
+            <img src="img/clients/tokopedia.png">
+          </div>
+          <div class="col m4 s12 center">
+            <img src="img/clients/traveloka.png">
+          </div>
+        </div>
+      </div> -->
+    <!-- </div> -->
+
+<!-- SERVICES -->
+  <section id="services" class="services blue lighten-5">
+    <div class="container">
+      <div class="row">
+        <h3 class=" center grey-text text-darken-3">Our Services</h3>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <i class="material-icons medium">home</i>
+            <h5>Design and Build Services</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorem dolorum nesciunt veniam fugit quam ex. Saepe consequuntur doloribus distinctio eveniet quis fugit voluptatem, aperiam tempore autem, iste, est voluptate?</p>
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <i class="material-icons medium">home</i>
+            <h5>Design and Build Services</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorem dolorum nesciunt veniam fugit quam ex. Saepe consequuntur doloribus distinctio eveniet quis fugit voluptatem, aperiam tempore autem, iste, est voluptate?</p>
+          </div>
+        </div>
+        <div class="col m4 s12">
+          <div class="card-panel center">
+            <i class="material-icons medium">home</i>
+            <h5>Design and Build Services</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorem dolorum nesciunt veniam fugit quam ex. Saepe consequuntur doloribus distinctio eveniet quis fugit voluptatem, aperiam tempore autem, iste, est voluptate?</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+
+
+  <!-- portofolio -->
+  <section id="portfolio" class="portfolio">
+    <div class="container">
+      <h3 class ="center grey-text text-darken-3">Projects</h3>
+      <div class="row">
+        <div class="col m3 s12">
+          <img src="img/portfolio/1.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/2.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/3.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/4.png" class="responsive-img materialboxed">
+        </div>
+      </div>
+       <div class="row">
+        <div class="col m3 s12">
+          <img src="img/portfolio/5.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/1.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/2.png" class="responsive-img materialboxed">
+        </div>
+        <div class="col m3 s12">
+          <img src="img/portfolio/3.png" class="responsive-img materialboxed">
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+<!-- paralax -->
+    <!-- <div class="parallax-container">
+      <div class="parallax"><img src="img/slider/4.png"></div>
+
+      <div class="container clients">
+        <h3 class="center light white-text">Our Client</h3>
+        <div class="row">
+          <div class="col m4 s12 center">
+            <img src="img/clients/gojek.png">
+          </div>
+          <div class="col m4 s12 center">
+            <img src="img/clients/tokopedia.png">
+          </div>
+          <div class="col m4 s12 center">
+            <img src="img/clients/traveloka.png">
+          </div>
+        </div>
+      </div> -->
+    <!-- </div> -->
+
+
+
+
+
+
+
+
+
+  <!-- contact us -->
+  <section id="contact" class="contact blue lighten-5">
+    <div class="container">
+      <h3 class="ligh grey-text text-darken-3 center"></h3>
+      <div class="row">
+        <div class="col m5 s12">
+          <div class="card-panel blue darken-2 center white-text">
+            <i class="material-icons medium">email</i>
+            <h5>Contact</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nam optio quas quisquam facilis perspiciatis.</p>
+          </div>
+          <ul class="collection with-header">
+            <li class="collection-header"><h5>Our Office</h5></li>
+            <li class="collection-item">SMCC UTAMA INDONESIA </li>
+            <li class="collection-item">Summitmas II 20th Floor, Jl. Jendral Sudirman Kav. 61-62, Jakarta Selatan, Indonesia </li>
+            <li class="collection-item">021-5201757</li>
+          </ul>
+          
+        </div>
+
+        <div class="col m7 s12">
+          <form>
+            <div class="card-panel">
+              <h5>Please fill out this form</h5>
+              <div class="input-field">
+                <input type="text" name="name" id="name" required >
+                <label for="name">Name</label>
+                
+              </div>
+              <div class="input-field">
+                <input type="email" name="email" id="email" class="validate">
+                <label for="email">Email</label>
+              </div>
+              <div class="input-field">
+                <input type="text" name="phone" id="phone">
+                <label for="phone">Phone</label>
+                
+              </div>
+              <div class="input-field">
+                <textarea name="message" id="message" class="materialize-textarea"></textarea>
+                <label for="message">Message</label>
+                
+              </div>
+              <button type="submit" class="btn blue darken-2">Send</button>
+            </div>
+            
+          </form>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+
+
+
+<!-- Footer -->
+<footer class="orange darken-2 white-text center">
+  <p >Copyright 2019. All Rights reserved</p>
+</footer>
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+
+      <script>
+        const sideNav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sideNav);
+
+        const slider = document.querySelectorAll('.slider');
+        M.Slider.init(slider,{
+          indicators: false,
+          height: 600,
+          transition: 600,
+          interval: 3000
+        });
+
+        const parallax = document.querySelectorAll('.parallax');
+        M.Parallax.init(parallax);
+
+
+        const materialbox = document.querySelectorAll('.materialboxed');
+        M.Materialbox.init(materialbox);
+
+        $(".dropdown-trigger").dropdown();
+      </script>
+    </body>
+  </html>
